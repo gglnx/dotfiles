227e5c68442fa090438fc2cff94e3cf1c6031bc1
Dennis Morhardt <info@dennismorhardt.de>
2020-12-08 22:20:29 +0100

    🐛 Prevent oh-my-zsh from killing the init script

--- init.zsh
+++ init.zsh
@@ -44,7 +44,7 @@ chsh -s $(which zsh)
 # Install oh-my-zsh
 if [ ! -x ~/.oh-my-zsh ]; then
     echo "Installing oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 fi
 
 # Install spaceship
